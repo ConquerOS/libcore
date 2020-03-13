AOSPA/android_libcore

Mask result with qNaN if input to ceil is NaN
This CL masks the output with qNaN for ceil of FP16 NaN values
i.e. mask the most significant mantissa bit with 1. In ARM hardware,
the rest of the mantissa bits are still processed when input is NaN,
therefore the mask comes at the end of the ceil function.

Test: cts-tradefed  run singleCommand cts-dev --module CtsUtilTestCases --test android.util.cts.FP16Test
Test: Exhaustive testing of 0x0..0xFFFF input bits between the java
      implementation and FRINTP ARM instruction
Change-Id: Id48a9f1c9afd0504d4f6d9d36d66a23a0e94c328

luni/src/test/java/libcore/libcore/util/FP16Test.java
@@ -174,6 +174,13 @@ public class FP16Test extends TestCase {
         assertEquals(-124.0f, toFloat(FP16.ceil(toHalf(-124.7f))), 1e-6f);
         assertEquals(125.0f, toFloat(FP16.ceil(toHalf(124.2f))), 1e-6f);
         assertEquals(-124.0f, toFloat(FP16.ceil(toHalf(-124.2f))), 1e-6f);
+        // ceil for NaN values
+        // These test check whether the ceil function acheives bit level
+        // compatibility with FRINTP ARM instruction
+        assertShortEquals((short) 0x7e01, FP16.ceil((short) 0x7c01));
+        assertShortEquals((short) 0x7f00, FP16.ceil((short) 0x7d00));
+        assertShortEquals((short) 0xfe01, FP16.ceil((short) 0xfc01));
+        assertShortEquals((short) 0xff00, FP16.ceil((short) 0xfd00));
     }
 
     public void testEquals() {

ConquerOS/libcore

Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

luni/src/test/java/libcore/libcore/util/FP16Test.java
@@ -167,6 +167,13 @@ public class FP16Test extends TestCase {
         assertEquals(-124.0f, toFloat(FP16.ceil(toHalf(-124.7f))), 0.0f);
         assertEquals(125.0f, toFloat(FP16.ceil(toHalf(124.2f))), 0.0f);
         assertEquals(-124.0f, toFloat(FP16.ceil(toHalf(-124.2f))), 0.0f);
+        // ceil for NaN values
+        // These tests check whether the current ceil implementation achieves
+        // bit level compatibility with the hardware implementation (ARM64)
+        assertEquals((short) 0x7e01, FP16.ceil((short) 0x7c01));
+        assertEquals((short) 0x7f00, FP16.ceil((short) 0x7d00));
+        assertEquals((short) 0xfe01, FP16.ceil((short) 0xfc01));
+        assertEquals((short) 0xff00, FP16.ceil((short) 0xfd00));
     }
 
     public void testEquals() {